060db723ae08ff0a1aee5e0ab9ec5c03b4ea2098
Hartvig1337 <paha19ab@student.cbs.dk>
2019-10-14 12:51:11 +0200

    Mucho ændringo

--- bruger.js
+++ bruger.js
@@ -1,21 +1,24 @@
 class User {
-    constructor(name, username, password, address, telephone, email) {
+    constructor(name, username, password, address, telephone, email, admin) {
         this.name = name;
         this.username = username;
         this.password = password;
         this.address = address;
         this.telephone = telephone;
         this.email = email;
+        this.admin = admin;
     }
 }
     // Insert create user function here
+BrugerTest = new User("Sanel","BrugerTest","123","Dalgas Have", "20202020","test@test.dk","False");
+
 function createUser() {
 
     var user = document.getElementById('username');
     var pass = document.getElementById('password');
 
-    var coruser = "test";
-    var corpass = "123";
+    var coruser = User.username;
+    var corpass = User.password;
 
     if(user.value == coruser) {
 
