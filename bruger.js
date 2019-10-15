53f886348935544a5ed57dcedb1048ea666cc8fc
Hartvig1337 <paha19ab@student.cbs.dk>
2019-10-15 11:59:57 +0200

    Log in function

--- bruger.js
+++ bruger.js
@@ -1,29 +1,26 @@
-/* class User {
-    constructor(realName, username, userPassword, address, telephone, email, admin) {
-        this.realName = realName;
-        this.username = username;
-        this.userPassword = userPassword;
-        this.address = address;
-        this.telephone = telephone;
-        this.email = email;
-        this.admin = admin;
+//Vi laver en User class som vi bruger til vores testbruger
+class User {
+    constructor(nm, pw) {
+        this.nm = nm;
+        this.pw = pw;
     }
 }
-//Testbrugere
-BrugerTest = new User("Sanel","test","123","Dalgas Have", "20202020","test@test.dk","False");
-*/
+//Testbruger(Ikke-admin)
+Testbruger = new User("Sanel","123");
 
+
+//Henter Username(nm) og password(pw) fra vores register form
 var nm = document.getElementById('nm');
 var pw = document.getElementById('pw');
 
-// Store user function
+// Store user funktionen - brugeren gemmes lokalt
 function StoreUser() {
     localStorage.setItem('nm', nm.value);
     localStorage.setItem('pw', pw.value);
     alert("Din bruger er oprettet - log ind i log ind feltet.");
 }
 
-// Insert create user function here
+// Henter localoprettet brugerinfo
 function Login() {
     var storedName = localStorage.getItem('nm');
     var storedPw = localStorage.getItem('pw');
@@ -31,13 +28,19 @@ function Login() {
     var userName = document.getElementById('userName');
     var userPw = document.getElementById('userPw');
 
+//Hvis den lokale brugers username og password er korrekt = log ind
     if(userName.value == storedName && userPw.value == storedPw) {
-        alert("jaa");
-    }
-    else {
-        alert("kæmpe fejl");
+        window.location.href = "http://localhost:63342/Projekt/Eksamensprojekt/BookingLB.html"
     }
 
+    // Hvordan får man den til at læse User class i stedet for Testbruger?
+//Hvis den prædefinerede testbruger(User) username og password er korrekt = log ind
+    else if(userName.value == Testbruger.nm && userPw.value == Testbruger.pw) {
+        window.location.href = "http://localhost:63342/Projekt/Eksamensprojekt/BookingLB.html";
+    }
+    else {
+        alert("Forkert brugernavn/password!");
+    }
 }
 
 // Insert delete user function here
