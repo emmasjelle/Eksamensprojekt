350f2c96b9905fb22e4957a534d58a5efd6c135f
Emma Sjelle <emsj19ab@student.cbs.dk>
2019-10-18 10:46:27 +0200

    Forsøg på array

---
+++ Booking.js
@@ -0,0 +1,22 @@
+class booking {
+    constructor(time, date, month) {
+        this.time = ["HEj", "john"];
+        this.date = date;
+        this.month = month;
+
+}
+}
+console.log(this.time)
+
+/*
+December5 = new booking("10:00-11:30",5,12);
+
+var dateArray = [];
+dateArray.push=(December5.date);
+var timearray = [];
+timearray.push=(December5.time);
+console.log(dateArray);
+
+
+
+*/
