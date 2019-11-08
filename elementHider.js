0d4cee21cb5e50f4537d34244123b013977108e0
Sanel Gluhic <sagl19ab@student.cbs.dk>
2019-11-08 12:49:57 +0100

    Rettet i forside og elementhider mht navigationsbar om hvornår den skjules og vises

---
+++ elementHider.js
@@ -0,0 +1,68 @@
+
+// Her afgøres hvilken af de 3 defineret navigationsbar vises på siden
+x=1
+if(x==0) {
+    document.getElementById("admintest").style.display = 'none';
+}
+
+if(x==1){
+    document.getElementById("user").style.display = 'none';
+}
+
+if(x==1){
+    document.getElementById("all").style.display = 'none';
+}
+
+
+
+//document.querySelectorAll(".NavigationBar");
+// Add a class to revome menu
+
+
+
+//NavigationBar[0].classList.add("hidden")
+//NavigationBar[0].classList.remove("hidden")
+
+
+
+
+// document.getElementById("NavigationBar").style.visibility = "hidden";
+
+
+
+
+
+
+
+
+
+// document.querySelector('#all').style.display = 'none';
+
+
+
+
+/*
+ var nav = document.getElementById('#all'),
+    body = document.body;
+
+ x = 1
+
+ if (x==0) {
+    (#alle).show;
+} else {
+    ("#all").hide;
+}
+
+if (x==0){
+    ("#user").show;
+} else {
+    ("#user").hide;
+}
+
+if (x==101){
+    ("#admin").show;
+} else {
+    ("#admin").hide;
+}
+
+ */
