1627f19ace5643456bc07e04157b5d5a7093ccd8
Sanel Gluhic <sagl19ab@student.cbs.dk>
2019-11-13 13:03:17 +0100

    ooooo

--- testA.js
+++ testA.js
@@ -1,4 +1,5 @@
 
+// loades på alle HTML sider
 function navBar() {
     // vi henter både sessionstorage og local storage brugerne.
 var activeUserNow = sessionStorage.getItem("activeUser");
@@ -11,7 +12,7 @@ var activeUserLocal = localStorage.getItem("admin");
     // Barene skuljes indtil der logges ind
     // Der laves et forloop som tjekker om den person som er logget ind, er logget ind som en admin
     for (var i = 0; i < users.length; i++) {
-        if (activeUserNow == users[i].un){
+        if (activeUserNow == users[i].un || activeUserLocal ){
                 if (users[i].admin == "true" || activeUserLocal == "true") {
                     /*show adm bar - hide all & user bar*/
                     document.getElementById("all").style.display = 'none';
@@ -20,7 +21,7 @@ var activeUserLocal = localStorage.getItem("admin");
                 }
                 // Der laves et forloop som tjekker om den person som er logget ind, er logget ind som en bruger
                 else {
-                    if (users[i].admin == "false"){
+                    if (users[i].admin == "false" || activeUserLocal == "false"){
                         document.getElementById("all").style.display = 'none';
                         document.getElementById("adm").style.display = 'none';
                         document.getElementById("user").style.display= 'block';
