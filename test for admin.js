d3df6e9106d669c5f69e560a17c715e5ecc7266d
Sanel Gluhic <sagl19ab@student.cbs.dk>
2019-11-13 11:20:42 +0100

    Hej

---
+++ test for admin.js
@@ -0,0 +1,21 @@
+
+function amdinlol {
+    onload = html siderne
+
+    for (var i = 0; i < users.length, i++) {
+        if (activeUser == users[i].admin == true); {
+            show navigation bar for admin
+        }
+    } if else (activeUser == users[i].admin == false); {
+        show navigation bar for user
+    }
+    else {
+        show navigation bar guest
+    }
+
+}
+
+
+kig users classes.js
+
+refere til classes.js
