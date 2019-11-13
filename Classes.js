40e7dbaa8ed155d7e6a6ce6b30cc07b783e4285d
Hartvig1337 <paha19ab@student.cbs.dk>
2019-11-13 13:09:17 +0100

    Log out function

--- Classes.js
+++ Classes.js
@@ -53,6 +53,7 @@ var email = document.getElementById('email');
 var animal = document.getElementById('animal');
 var admin = document.getElementById('admin');
 
+//When creating a Practitioner in Local Storage
 var adminKey = "888";
 
 //Function that stores the registerinformation
@@ -75,44 +76,4 @@ function testForAdmin(){
             localStorage.setItem('admin', 'false');
             alert("Din bruger er oprettet - log ind i log ind feltet.");
         }
-}
-
-let activeUser = "";
-
-function Login() {
-    var storedName = localStorage.getItem('un');
-    var storedPw = localStorage.getItem('pw');
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPw');
-/*  Three posibiliteis here: 1. A pre-made user succesfully logs in(from users array). 2. A newly created user logs in(Local storage).
-    3.Username or password from pre-made or new user is incorrect = acces denied */
-    var valid = false;
-    var invalid = true;
-    // Forloop that looks through the users array for matching usernames and then passwords for the matching index
-    for (var i = 0; i < users.length; i++) {
-        if (userName.value == users[i].un && userPw.value == users[i].pw || userName.value == storedName && userPw.value == storedPw) {
-            valid = true;
-        }
-    }
-    if (valid) {
-        window.location.href = "BookingLB.html";
-        sessionStorage.setItem("activeUser", userName.value);
-    }
-    else if (invalid) {
-        alert("Brugernavnet \""+userName.value+"\" eller password er forkert.");
-        return true;
-        sessionStorage.setItem("activeUser", "");
-    }
-}
-
-// Return user to log in page if not logged in SPØRG MIKKEL
-//Se Session storage ifht. isLoggedIn - I stedet for logIn så sæt en active user
-function isLoggedIn() {
-  /* localStorage.getItem("logIn");
-    if (logIn == "0") {
-        // window.location.href = "LogIn.html";
-    } */
-}
-function logOut(){
-    sessionStorage.clear();
 }
