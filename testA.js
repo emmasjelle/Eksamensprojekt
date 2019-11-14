937dc4edbc146d784e47a3ec8e42be9dd2dd7077
Hartvig1337 <paha19ab@student.cbs.dk>
2019-11-14 15:54:45 +0100

    function navBar() virker nu både i pre og localStorage

--- testA.js
+++ testA.js
@@ -1,23 +1,27 @@
 // Loaded(onload in body) on all HTML pages to check which Navigation Bar to display
 
 function navBar() {
-    // When a pre-defined user or user from localStorage logs in, the system stores their unique username as an activeUser in sessionStorage.
-    // sessionStorage is instead of localStorage, in order to prevent the system from saving the user as being logged in after the session is terminated.
+    /* When a pre-defined user or user from localStorage logs in, the system stores their unique username
+    as an activeUser in sessionStorage. SessionStorage is instead of localStorage, in order to prevent the system from
+    saving the user as being logged in after the session is terminated. */
     var activeUserNow = sessionStorage.getItem("activeUser");
     //The variables below are used in order to check the admin boolean value of a localStorage created user.
     var localUserNow = localStorage.getItem("un");
     var localUserAdmin = localStorage.getItem("admin");
 
     //There is no current activeUser if the sessionStorage is empty (... == 0).
-    //As the function loads on every page, the system knows that an empty sessionStorage should result in the "all"(guest) Navigation Bar to be displayed.
+    /*As the function loads on every page, the system knows that an empty sessionStorage should result in
+    the "all"(guest) Navigation Bar to be displayed. */
     if (sessionStorage.length == 0) {
-        //We are using style.display to hide the Navigation options, that should not be accesible for the unregistered guest.
+        /*We are using style.display to hide the Navigation options, that should not be
+        accesible for the unregistered guest. */
         document.getElementById("user").style.display = 'none';
         document.getElementById("adm").style.display = 'none';
     }
-    //If there is an activeUser(admin true/false) the script runs the activeUserNow through a loop checking the users array.
-    //after a match is found between activeUser(username) and the usernames in the user array through an if statement, another if statement is triggered.
-    //the next if statement checks if the indexed user[i] has an admin boolean value of "true".
+    /*If there is an activeUser(admin true/false) the script runs the activeUserNow through a loop checking
+    the users array. After a match is found between activeUser(username) and the usernames in the user array
+    through an if statement, another if statement is triggered. The next if statement checks if the indexed
+    users[i] has an admin boolean value of "true". */
     for (var i = 0; i < users.length; i++ ) {
         if (activeUserNow == users[i].un) {
             if (users[i].admin == "true") {
@@ -26,8 +30,9 @@ function navBar() {
                 document.getElementById("user").style.display = 'none';
             }
         }
-        //If the sessionStorage is not empty, and the activeUserNow does not have the boolean value of "true", a series of if statements is trigged like above.
-        //This time the statement checks if the activeUserNow has a boolean value of "false", resulting in the "user"(client) Navigation Bar being shown.
+        /*If the sessionStorage is not empty, and the activeUserNow does not have the boolean value of "true",
+        a series of if statements is trigged like above. This time the statement checks if the activeUserNow has a
+        boolean value of "false", resulting in the "user"(client) Navigation Bar being shown. */
         if (activeUserNow == users[i].un) {
             if (users[i].admin == "false") {
                 document.getElementById("all").style.display = 'none';
@@ -35,7 +40,8 @@ function navBar() {
             }
         }
     }
-    //The if statments below work like the ones above. The only difference is the comparison between the current activeUserNow and the localUserNow instead of the user array.
+    /*The if statments below work like the ones above. The only difference is the comparison between the current
+    activeUserNow and the localUserNow instead of the user array. */
     if (activeUserNow == localUserNow && localUserAdmin == "true") {
         document.getElementById("all").style.display = 'none';
         document.getElementById("user").style.display = 'none';
