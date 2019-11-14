a27c2b4a07c8f97f4ee1faa8831e5a006196b9cb
Hartvig1337 <paha19ab@student.cbs.dk>
2019-11-14 18:52:48 +0100

    font-type rettet i css

--- TESTJS.js
+++ TESTJS.js
@@ -1,34 +1,141 @@
-// Oprettelse af class
-class booking {
-    constructor(year, month, date) {
-        this.year = year;
-        this.month = month;
-        this.date = date;
-    }
-}
-//Her oprettes ledige bookingtider, 5,6,7, december.
-Oktober23 = new booking(2019, 10, 23);
-December5 = new booking(2019, 12, 5);
-December6 = new booking(2019, 12, 6);
-December7 = new booking(2019, 12, 7);
+//Sanels gamle Booking HTML
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <link href='Style.css' rel='stylesheet'>
+    <script type="text/javascript" src="Classes.js"></script>
+    <script type="text/javascript" src="testA.js"></script>
+    <title>Booking</title>
+    </head>
 
-var times = [];
-times.push(Oktober23, December5, December6, December7);
+    <body onload="isLoggedIn(); navBar()">
 
-function searchDate() {
-    var loadedDate = new Date(document.getElementById("choosenDate").value);
-    if(choosenDate.value == "2019-10-23"){
-        alert("lol");
-    }
-}
+    <!--Navigationbars-->
+
+    <!-- logo -->
+    <img src="https://i.imgur.com/AiPTQKP.png"
+style="width:13%;height:20%;float:right;padding:5px">
+
+    <div class="NavigationBar" id="all">
+
+    <ul>
+    <!--Fælles navigationsbar-->
+<li><a href="Forside.html">Forside</a></li>
+<li><a href="BehandlingsTyper.html">Behandlingstyper</a></li>
+<li><a href="LogIn.html">Booking</a></li>
+<li><a href="omMig.html">Om mig</a></li>
+<li><a href="Kontakt.html">Kontakt</a></li>
+</ul>
+</div>
+
+<div class="NavigationBar" id="user">
+    <ul>
+    <!--Bruger navigationbar-->
+<li><a href="Forside.html">Forside</a></li>
+<li><a href="BehandlingsTyper.html">Behandlingstyper</a></li>
+<li><a href="BookingLB.html">Booking</a></li>
+<li><a href="omMig.html">Om mig</a></li>
+<li><a href="Kontakt.html">Kontakt</a></li>
+<!--Bruger funktioner-->
+<li><a href="seTiderB.html">Se mine tider</a></li>
+<li><a onClick="logOut()" href="Forside.html">Log ud</a></li>
+</ul>
+</div>
+
+<div class="NavigationBar" id="adm">
+    <ul>
+    <!--Admin navigationbar-->
+<li><a href="Forside.html">Forside</a></li>
+<li><a href="BehandlingsTyper.html">Behandlingstyper</a></li>
+<li><a href="BookingLB.html">Booking</a></li>
+<li><a href="omMig.html">Om mig</a></li>
+<li><a href="Kontakt.html">Kontakt</a></li>
+<!--Admin funktioner-->
+<li><a href="tilfoejTiderA.html">Tilføj tider</a></li>
+<li><a href="seBooketTiderA.html">Se booket tider</a></li>
+<li><a onClick="logOut()" href="Forside.html">Log ud</a></li>
+</ul>
+</div>
+
+<h1>Velkommen til vores bookingside</h1>
+
+<link rel="stylesheet" href="Calender.css" />
+
+    <div class="date-picker">
+    <div class="selected-date"></div>
+
+    <div class="dates">
+    <div class="month">
+    <div class="arrows prev-mth">&lt;</div>
+    <div class="mth"></div>
+    <div class="arrows next-mth">&gt;</div>
+    </div>
+
+    <!-- Hvorfor virker det ikke med id -> ligemeget hvor man trykker vil den tælle op onclick="displayTime()"-->
+    <div id="henrikthorn" class="days">
+    <div class="day">1</div>
+    <div class="day">2</div>
+    <div class="day">3</div>
+    <div class="day">4</div>
+    <div class="day">5</div>
+    <div class="day">6</div>
+    <div class="day">7</div>
+    </div>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <table>
+    <tr><td><button class="editbtn">edit</button></td></tr>
+<tr><td><button class="editbtn">edit</button></td></tr>
+<tr><td><button class="editbtn">edit</button></td></tr>
+<tr><td><button class="editbtn">edit</button></td></tr>
+</table>
+
+<!--  Leilas tabel som popper op -> virker sku ik
+<div>
+<table id="hello">
+    <thead>
+    <tr>
+    <th><b>Ledige tider</b></th>
+</tr>
+</thead>
+<tbody id = "tbody">
+    </tbody>
+    <tfoot>
+    </tfoot>
+    </table>
+    </div>
+-->
 
 
 
-/*
-// booking array for time, month, date,
 
-//Vi laver en en ny ledig tid d: 23/03/2019 med tiderne defineret i funtionen.
-test = new booking(2, 23, 3);
 
-console.log(test.time); //Her laves en instance of a class for at teste tiderne
-*/
+<!--
+    Velkommen til vores bookingside
+<br/>
+Vælg en dato
+<input type="date" id="choosenDate" value="">
+    <input type="button" value="Søg på dato" onClick="searchDate()">
+    -->
+
+    <script src="Calender.js"></script>
+    <script type="text/JavaScript" src="Booking.js"></script>
+    <script type="text/JavaScript" src="logIn.js"></script>
+    </body>
+
+    </html>
