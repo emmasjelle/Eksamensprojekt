6221c1291e4cdc3e79b8d19d253f17e88eca037a
Hartvig1337 <paha19ab@student.cbs.dk>
2019-12-02 09:54:04 +0100

    Upload AFTER merge

---
+++ CheckLogIn.js
@@ -0,0 +1,22 @@
+ function isLoggedIn() {
+    let check = sessionStorage.length;
+    console.log(check);
+    switch (check) {
+        case 0:
+            window.location.href = "LogIn.html";
+            break;
+        case 1:
+            window.location.href = "Bookingside.html";
+            break;
+        default:
+            console.log('Default');
+            break;
+     }
+ }
+ /*   }
+    if (sessionStorage.length == 0) {
+        window.location.href = "LogIn.html";
+    } else {
+        window.location.href = "Bookingside.html";
+    }
+} */
