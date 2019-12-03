e4c7a60e1740804ca0d75aa1384fe0c8e23f759d
Emma Sjelle <emsj19ab@student.cbs.dk>
2019-12-03 10:53:56 +0100

    Commenting done. Admin-validate not working.

--- Classes.js
+++ Classes.js
@@ -1,6 +1,7 @@
-//Denne JS-fil indeholder vores User class (Parent), underklasser, register funktionen og tilhørende funktioner.
+//The purpose of this code is to create our parent and children classes, validate the information given in the
+//register form and finally store the created user in local storage.
 
-//Parent User klassen i vores system. Vi har dertil en constructor som indeholder variabler/attributes/properties?
+//Parent User class in our system. The constructor contains variables which applies for both types of user
 class User {
     constructor(un, pw, nm, uAddress, phoneNumber, email, admin) {
         this.un = un;
@@ -12,14 +13,14 @@ class User {
         this.admin = admin;
     }
 }
-//Behandler/practitioner (admin) User klassen fra vores parent User
+//Practitioner (admin) class which inherit from the parent class. This also applies for the next class "Client". In
+// order to inherit, we use "extends User".
 class Practitioner extends User {
     constructor(un, pw, nm, uAddress, phoneNumber, email, admin, infoAboutMe) {
         super(un, pw, nm, uAddress, phoneNumber, email, admin);
-        this.infoAboutMe = infoAboutMe;
     }
 }
-//Klient/Client User klassen fra vores parent User
+
 class Client extends User {
     constructor(un, pw, nm, uAddress, phoneNumber, email, admin, Animal) {
         super(un, pw, nm, uAddress, phoneNumber, email, admin);
@@ -27,10 +28,14 @@ class Client extends User {
     }
 }
 
-//Her opretter vi funktionen som vil tjekke at alle felter i brugerregisteringen bliver udfyldt korrekt.
-// Vi benytter getElementById til at hente værdierne gennem deres ID. Derudover bruger vi .value
-// Vi har udkommenteret fra uAdress til admin, da værdierne på nuværende tidspunkt ikke bliver brugt når vi opretter en
-//bruger. Vi har derfor også udkommenteret de tilhørende if-statements. 
+//When you create a practitioner in local storage this admin-key must be used during creation.
+var adminKey = "888";
+
+//This function checks that all fields in the registerform is completed correctly. We use the getElementByID to collect
+//the values trough the ID and the .value enables us to collect the given value we get from the HTML form.
+//Also we haven chosen to commented out from "uAddress" to "email" due to the lack of use when creating a user. This is
+//also why we commented out the related if-statements.
+
  function validate() {
     var un = document.getElementById("un").value;
     var pw = document.getElementById("pw").value;
@@ -38,57 +43,61 @@ class Client extends User {
     //var uAddress = document.getElementById("uAddress").value;
     //var phoneNumber = document.getElementById("phoneNumber").value;
     //var email = document.getElementById("email").value;
-    //var admin = document.getElementById("admin").value;
+    var admin = document.getElementById("admin");
 
-//Vi laver if-statement til at kunne specificere hvad hver enkelt felt skal indeholde for at kunne returnere true (blive
-// "godkendt".)
+//In order to specify what every single field should contain to return true, we create if-statements.
 
-   //Her bruger vi .length til at tjekke længden på brugernavnet og passwordet
-    if (un.length < 5) {
-        alert("Dit brugernavn skal indeholde minimum 5 karakterer");
+   //We use .length in order to check the lenght of the username and password.
+    if (un.length < 4) {
+        alert("Dit brugernavn skal indeholde minimum 4 karakterer");
         return false;
     }
-    if (pw.length < 5) {
-        alert("Dit password skal indeholde minimum 5 karakterer");
+    if (pw.length < 8) {
+        alert("Dit password skal indeholde minimum 8 karakterer");
         return false;
     }
 
-//Her bruger vi en equals operator til at sørge for, at brugernavns feltet bliver udfyldt.
+//We use the equals operator to make sure, what that the username field is being filled out.
     if (nm.value == "") {
         alert("Brugernavn kræves");
         return false;
     }
-//Her bruger vi først en NaN funktion, som betyder at det skal være tal, som indtastes. Derefter bruger vi en
-// and-operater og en notequals operator til at definere hvordan et telefonnummer skal skrives.
+//Firstly we use the NaN function, which demands the field to be entered with numbers. Afterwards we use and-operator
+// and notequals operator in order to define how a phonenumber should be written.
     /*if(isNaN(phoneNumber) && phoneNumber.length != 8){
-        alert("Indtast venligst et gyldigt telefonnummer");
+        alert("Indtast venligst et gyldigt dansk telefonnummer");
            return false;
      }
 
-//Her bruger vi indexOf("@") og equals operator til at definere at dette felt skal indeholde ét @. Derudover bruger vi
-//igen .lenght til at definere minimum længden på emailen.
+//We use the indexOf("@") and equals operator to define, that this field should contain one @.
      if(email.indexOf("@") == -1 || email.length < 5){
          alert("Indtast en gyldig email");
          return false;
      }*/
 
-//Hvis den løber igennem alle if-statements og de ikke returnerer false, så vil den køre hhv. StoreUser() og
-// testForAdmin() funktionerne.
+    //Not working. Why??
+     if (admin.value != adminKey || admin.value != null) {
+         alert("Forkert admin nøgle");
+         return false;
+     }
+
+//If it runs through all if-statements and they don't return false, it will finally run the StoreUser() and
+// testForAdmin() functions.
     StoreUser();
     testForAdmin();
     return true;
 }
 
-//Her opretter vi en behandler/practitioner test bruger.
+//We create a practitioner test user.
 PractitionerTest = new Practitioner("Sanel","123","Sanel Gluhic","Dalgas Have 1","12345678","sanel@cbs.dk","true","");
-//Her opretter vi en klient/client test bruger.
+//client test user.
 ClientTest = new Client("Emma","123","Emma Sjelle","Dalgas Have 2","12345677","emma@cbs.dk","false","Horse");
 
-//Her opretter vi et tomt array, hvor vi pushe vores testbrugere op i.
+//We create an empty array for the test users.
 let users = [];
 users.push(PractitionerTest,ClientTest);
 
-//Her hentes informationer fra registrerformen.
+//Information recived from the register form.
 var un = document.getElementById('un');
 var pw = document.getElementById('pw');
 var nm = document.getElementById('nm');
@@ -98,10 +107,9 @@ var email = document.getElementById('email');
 var animal = document.getElementById('animal');
 var admin = document.getElementById('admin');
 
-//Når man opretter en behdandler/practitioner i local storage skal denne admin-nøgle bruges for oprettelse.
-var adminKey = "888";
 
-//Vi laver en funktion som gemmer den information vi har modtaget i registrer bruger formen.
+
+//This function stores the information received in the register user form.
 function StoreUser() {
     localStorage.setItem('un', un.value);
     localStorage.setItem('pw', pw.value);
@@ -112,8 +120,7 @@ function StoreUser() {
     localStorage.setItem('animal', animal.value);
     localStorage.setItem('admin', admin.value);
 }
-//Her opretter vi en funktion som bruger en if og en else statement til at teste om der er blevet oprettet en admin-bruger
-// eller om der er blevet oprettet en almindelig bruger. Derudover bruger vi igen en equals operator.
+//This is + else statement test  if the created user is an admin or client user.
 function testForAdmin(){
     if(admin.value == adminKey) {
         localStorage.setItem('admin', 'true');
@@ -124,8 +131,8 @@ function testForAdmin(){
             alert("Din bruger er oprettet - du kan nu logge ind.");
         }
 }
-//Her eksperimenterede vi med at pushe local storage brugere til det samme array, som vores præ-definerede bruger er I.
-//Det fungerede dog ikke.
+//We tried to experiment with pushing local storage users to the same array, as our pre defined users are in. Didn't
+// work.
 
 /*function localtoArray() {
     var localAdmin = localStorage.getItem('admin');
@@ -142,8 +149,7 @@ function testForAdmin(){
 }
 */
 
-//Vi oprettede denne funktion til at kunne clear vores Local Storage fra vores browser consol. Vi har især brugt denne
-//funktion under vores tests.
+//In order to clear our local storage from the browser console. Very efficient during test.
 function clearLocal(){
     localStorage.clear();
 }
