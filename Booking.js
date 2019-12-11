41adc87720c1381dec43363adc6d6f61c18e21d1
Sanel Gluhic <sagl19ab@student.cbs.dk>
2019-12-11 10:04:04 +0100

    Hej

--- Booking.js
+++ Booking.js
@@ -17,7 +17,7 @@ class Booking {
 //10/12/2019 times
 Test1 = new Booking("0","10/12/2019","08:00","10:00","Sanel", true, 1,"");
 Test2 = new Booking("1","10/12/2019","11:00","13:00","Sanel", true,2,"");
-Test3 = new Booking("2","10/12/2019","14:00","16:00","Sanel", true,3,"");
+Test3 = new Booking("2","10/12/2019","14:00","16:00","Sanel", true,3,"g");
 //11/12/2019 times
 Test4 = new Booking("3","11/12/2019","08:00","10:00","Sanel", true,1,"");
 Test5 = new Booking("4","11/12/2019","11:00","13:00","Sanel", true,2, "");
