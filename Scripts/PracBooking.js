81b791cb5bd4c17002854a2161f62a559dd2e1f4
Emma Sjelle <emsj19ab@student.cbs.dk>
2020-05-06 09:36:38 +0200

    Rettelse til tilføj dyr + practioner kan oprette bookings

---
+++ Scripts/PracBooking.js
@@ -0,0 +1,28 @@
+function createBooking() {
+    let booking_date = document.getElementById('date');
+    let booking_start = document.getElementById('start');
+    let booking_slut = document.getElementById('slut');
+    //Denne del henter den aktive brugers id
+    let userEmail = sessionStorage.activeUser;
+    const body = {email: userEmail};
+    axios.post('http://localhost:3000/users/check', body)
+        .then((response) => {
+            let ownerId = response.data.id;
+            const body2 = {
+                date: booking_date.value,
+                time: booking_start.value+'-'+booking_slut.value,
+                practitioner: ownerId,
+                client: ownerId,
+                animal: undefined
+            };
+            //Her oprettes bookingen som ledig i systemet under den tilhørende practitioner
+            axios.post('http://localhost:3000/bookings/create', body2)
+                .then((res) => {
+                    showBookings();
+                })
+        })
+        .catch((err) => {
+            //Denne catch skal fange min medelelse fra api'en
+            console.log(err)
+        })
+}
